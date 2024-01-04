fb12c8ad04447a86140d7fc9b9f61eb9322f3a68
serialine <7322570+serialine@users.noreply.github.com>
2024-01-04 17:24:32 +0900

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+import { ref } from 'vue';
+import { defineComponent } from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  setup() {
+    const title = ref('Hello nuxt 2.7 ts');
+    return { title };
+  },
+});
+</script>
+
+<template>
+  <div class="container">
+    {{ title }}
+  </div>
+</template>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
